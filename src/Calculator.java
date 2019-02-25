04780f5ed6baaa704e641484bedf320786c858fe
Brandon <brandon.aboite@e3civichigh.com>
2019-02-24 16:15:55 -0800

    Section 3 activity completed

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,17 +2,46 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        double num1;
+        double num2;
+        double result=0;
+        int whichCalculation;
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        System.out.print("What operation? (+, -, *, /):");
-        String operation = scanner.next();
+        System.out.println("Choose a task. Press 1 to add, 2 to subtract, 3 to multiply, 4 to divide");
 
-        System.out.print("What is the first number? ");
-        double firstNumber = scanner.nextDouble();
+        whichCalculation = input.nextInt();
 
-        System.out.print("What is the second number? ");
-        double secondNumber = scanner.nextDouble();
+        System.out.println("Please enter the first number");
+        num1 = input.nextDouble();
+
+        System.out.println("Please enter the second number");
+        num2 = input.nextDouble();
+
+        if(whichCalculation == 1){
+            result = num1 + num2;
+
+        }
+
+        if(whichCalculation == 2){
+            result = num1 - num2;
+
+        }
+
+        if(whichCalculation == 3){
+            result = num1 * num2;
+
+        }
+
+        if(whichCalculation == 4){
+            result = num1 / num2;
+
+        }
+
+        System.out.println("The answer is...");
+        System.out.println(result);
+
 
     }
 
